ec4c2c757ed02cc8ebd4baa377df59bb870d4edd
hamishliu <hamish.mc.liu@gmail.com>
2022-11-10 21:56:05 +0900

    added comments

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,13 +4,21 @@ import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  // allows username and age to become valid input
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    // ensures empty forms are not submitted
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     console.log(enteredUsername, enteredAge);
-    setEnteredUsername('');
+    setEnteredUsername('')
     setEnteredAge('')
   };
 
@@ -23,12 +31,24 @@ const AddUser = (props) => {
   };
 
   return (
+    // takes the class component with its css
     <Card className={classes.input}>
+      {/* form inputs new value when submitted */}
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" value={enteredUsername} onChange={userNameChangeHandler}></input>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={userNameChangeHandler}
+        ></input>
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}></input>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit"> Add user</Button>
       </form>
     </Card>
