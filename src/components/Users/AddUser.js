24ba68351281a620fddc66fdf48906c7a3a3824d
hamishliu <hamish.mc.liu@gmail.com>
2022-11-10 19:27:27 +0900

    added a card to wrap the AddUser form, this card takes css from the card.css and the AddUser.css

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
+
+import Card from '../UI/Card'
+import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
-    <form onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
         <input id="username" type="text"></input>
-        <label htmlFor='age'>Age</label>
+        <label htmlFor="age">Age</label>
         <input id="age" type="number"></input>
         <button type="submit"> Add user</button>
-    </form>
-  )
-}
+      </form>
+    </Card>
+  );
+};
 
-export default AddUser
+export default AddUser;
